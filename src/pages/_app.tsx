0c3acd8fcaf880327f3ca9901a49302cb455246f
notnulldev <notnulldev0@gmail.com>
2022-11-24 23:29:58 +0100

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,109 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+
+import { trpc } from "../utils/trpc";
+
+import "../styles/globals.css";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <div className="container mx-auto h-screen">
+        <Header />
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between p-6">
+      <h1 className="text-2xl">The shop</h1>
+      <div className="flex gap-2">
+        <input className="input" placeholder="I am looking for..." />
+
+        <div className="dropdown">
+          <div className="input-group">
+            <input
+              tabIndex={0}
+              className="input"
+              placeholder="In category..."
+            />
+            <button className="btn-square btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu rounded-box mt-2 w-52 bg-base-100 p-2 shadow"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-6">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-8 w-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-8 w-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+          />
+        </svg>
+
+        <div className="placeholder avatar">
+          <div className="w-10 rounded-full bg-neutral-focus text-neutral-content">
+            <span className="text-3xl">J</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default trpc.withTRPC(MyApp);
