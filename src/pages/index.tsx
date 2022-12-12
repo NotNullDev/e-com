3add82828c5ac01e18d2394c0bf84ca2d3f7a2c0
notnulldev <61083452+NotNullDev@users.noreply.github.com>
2022-12-12 05:08:44 +0100

    added skeleton to the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,6 +61,8 @@ const Home: NextPage = () => {
                 ))}
               </>
             )}
+            {filteredProducts.status === "loading" &&
+              [...Array(20)].map((i) => <SingleProductSkeletion key={i} />)}
           </div>
         </div>
       </div>
@@ -145,6 +147,19 @@ const SingleProductPreview = ({ product }: SingleProductPreviewProps) => {
         <SingleProductSticker dealType={product.dealType} />
       </Link>
     </>
+  );
+};
+
+const SingleProductSkeletion = () => {
+  return (
+    <div className="flex h-[300px] w-[250px] animate-pulse flex-col rounded shadow-md">
+      <div className="h-48 rounded-t bg-base-100"></div>
+      <div className="flex-1 space-y-4 bg-base-300 px-4 py-8 sm:p-8">
+        <div className="h-6 w-full rounded bg-base-100"></div>
+        <div className="h-6 w-full rounded bg-base-100"></div>
+        <div className="h-6 w-3/4 rounded bg-base-100"></div>
+      </div>
+    </div>
   );
 };
 
