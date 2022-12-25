d261f3130def387e5d94473174a752c9abde4bc1
notnulldev <61083452+NotNullDev@users.noreply.github.com>
2022-12-25 08:18:03 +0100

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-env
+ls -lah
 
-ls
-
-npx prisma db push
-npx prisma db seed
+if test "$INIT_DB" = "true"; then
+  echo "Initializing database"
+  npx prisma db push
+  npx prisma db seed
+fi
 
 node server.js
