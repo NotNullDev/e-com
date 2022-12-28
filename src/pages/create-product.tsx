887d9e24949883bec0e49c6a9f086e089a274428
notnulldev <notnulldev0@gmail.com>
2022-12-28 02:06:00 +0100

    up

--- src/pages/create-product.tsx
+++ src/pages/create-product.tsx
@@ -22,8 +22,7 @@ import { getAllCategoriesAsString } from "../utils/enumParser";
 import { trpc } from "../utils/trpc";
 
 export const NEXT_PUBLIC_IMAGE_SERVER_URL =
-  // "https://file-server.notnulldev.com";
-  "file-server.localhost";
+  "https://file-server.notnulldev.com";
 
 const CreateProductPage = () => {
   return (
